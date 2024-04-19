683d90d3d4653b31646fecd9ba52178011aa66ee
Adrian <88957069+JoseAdrianRodriguezGonzalez@users.noreply.github.com>
2024-04-19 11:20:11 -0600

    Update script.R
    Añadir la grafica

--- Analysis/script.R
+++ Analysis/script.R
@@ -1,1 +1,11 @@
-print("hello world");
+library(lubridate)
+DATASET<-read.csv('./dataset/DATASET.CSV')
+plotanydata<-function(dataframe){
+    DATASET$DATE=format(as.POSIXct(DATASET$DATE,format="%d/%m/%Y %H:%M"))
+    DATASET$date=as.Date(DATASET$DATE)
+    DATASET$time=format(as.POSIXct(DATASET$DATE),format="%H:%M")
+    h <- seconds(hm(DATASET$time))
+    plot(h, DATASET$Temperature)
+    plot(h, DATASET$Humidity)
+}
+plotanydata(DATASET)
